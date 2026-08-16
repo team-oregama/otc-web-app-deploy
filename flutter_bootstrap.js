018a6cb794e62c6dbf6c44da04d018fa3afa5ac6
deploy-bot-team-oregama <deploybot@teamoregma.noreply.github.com>
2026-08-16 19:15:24 +0000

    chore(deploy): web build 049c76c (run #40, version 1.0.0+40)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2410704987" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2267956224" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
